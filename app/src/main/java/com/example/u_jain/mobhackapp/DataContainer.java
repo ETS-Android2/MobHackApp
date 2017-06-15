cee5eb7d0256bcfca7d0ff93ebc6dc8955b106c7
jainudi48 <jain.udit48@gmail.com>
2017-06-15 05:39:38 +0530

    DataContainer DS in progress
    Data Structure for parameters data is in progress

--- app/src/main/java/com/example/u_jain/mobhackapp/DataContainer.java
+++ app/src/main/java/com/example/u_jain/mobhackapp/DataContainer.java
@@ -1,9 +1,31 @@
 package com.example.u_jain.mobhackapp;
 
+import java.util.HashMap;
+
 /**
  * Created by U_Jain on 6/15/2017.
  */
 
 public class DataContainer {
-    
+
+    DataEntity entities[] = new DataEntity[5];
+    final String criticalities[] = {"high", "medium high", "medium", "medium low", "low"};
+
+    public class DataEntity {
+        String citicalityMeasure;
+        HashMap<String, String> paramHashMap;
+    }
+
+
+    public DataContainer()
+    {
+        for(int i=0; i<5; i++)
+        {
+            entities[i].citicalityMeasure = criticalities[i];
+            entities[i].paramHashMap = new HashMap<>();
+            
+        }
+    }
+
+
 }
